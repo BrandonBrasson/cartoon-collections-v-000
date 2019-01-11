b741ec882447606389d982c1f651087809e0f787
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 02:13:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,9 +6,10 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
+ planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
   planeteer_calls.collect do |call|
     call.capitalize << "!"
-  end
+ end
 end
 
 def long_planeteer_calls(calls)
